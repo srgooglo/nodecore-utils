0c4d84b3ac4e5b9bc93864d981fdab2fb26f70b0
srgooglo <chippotato39@gmail.com>
2020-11-06 08:57:52 +0100

    added error handler

--- src/verbosity.ts
+++ src/verbosity.ts
@@ -40,99 +40,103 @@ export default <obj extends Readonly<Obj>>(data: obj["data"], params?: obj["para
         time: true
     }
 
-    const frame = stackTrace.get()[1]
+    try {
+        const frame = stackTrace.get()[1]
 
-    const stackTraceData = {
-        time: new Date().toLocaleTimeString(),
-        line: `(:${frame.getLineNumber()})`,
-        file: path.basename(frame.getFileName()),
-        method: `[${frame.getFunctionName()}]`
-    }
-
-    if (typeof (params) !== "undefined" || params != null) {
-        // @ts-ignore
-        __legacy__objectToArray(params).forEach((e: any) => {
-            if (typeof (e.value) !== "undefined") {
-                // @ts-ignore
-                opt[e.key] = e.value
-            }
-        })
-    }
-
-    if (typeof (stackTraceParams) !== "undefined" || stackTraceParams != null) {
-        // @ts-ignore
-        __legacy__objectToArray(stackTraceParams).forEach((e: any) => {
-            if (typeof (e.value) !== "undefined") {
-                // @ts-ignore
-                optStackTrace[e.key] = e.value
-            }
-        })
-    }
-
-    const getColor = (data: any, index: number) => {
-        let thisColor = opt.color[index]
-        let thisBackground = opt.secondColor[index]
-
-        if (thisBackground) {
-            // @ts-ignore
-            return chalk[thisColor][thisBackground](data)
+        const stackTraceData = {
+            time: new Date().toLocaleTimeString(),
+            line: `(:${frame.getLineNumber()})`,
+            file: path.basename(frame.getFileName()),
+            method: `[${frame.getFunctionName()}]`
         }
 
-        if (thisColor) {
+        if (typeof (params) !== "undefined" || params != null) {
             // @ts-ignore
-            return chalk[thisColor](data)
-        }
-
-        return data
-    }
-
-    const stackTraceKeys = Object.keys(optStackTrace)
-    const stackTraceLength = stackTraceKeys.length
-    let modifyCount = 0
-    let tmp
-
-    for (let i = 0; i < stackTraceLength; i++) {
-        const key = stackTraceKeys[i]
-        // @ts-ignore
-        const stackTraceDataKey = `${stackTraceData[key]}`
-        const divisor = getColor((i == (stackTraceLength - 1) ? " | " : " > "), 0)
-
-        // console.log(`[${key}] is the ${i == stackTraceLength? "last opt" : `n[${i}]` }`)
-        // console.log(i, "/", stackTraceLength -1)
-
-        // @ts-ignore
-        if (typeof (stackTraceData[key]) !== "undefined" && optStackTrace[key]) {
-            if (Array.isArray(data)) {
-                if (modifyCount == 0) {
-                    tmp = (getColor(stackTraceDataKey, 0) + divisor)
-                } else {
-                    tmp = (getColor(stackTraceDataKey, 0) + divisor + tmp)
+            __legacy__objectToArray(params).forEach((e: any) => {
+                if (typeof (e.value) !== "undefined") {
+                    // @ts-ignore
+                    opt[e.key] = e.value
                 }
-                if (i == (stackTraceLength - 1)) {
-                    let mix: any[] = []
-                    data.forEach((element: any) => {
-                        if (opt.color[1] || opt.secondColor[1]) {
-                            if (typeof (element) == "string") {
-                                return mix.push(getColor(element, 1))
+            })
+        }
+
+        if (typeof (stackTraceParams) !== "undefined" || stackTraceParams != null) {
+            // @ts-ignore
+            __legacy__objectToArray(stackTraceParams).forEach((e: any) => {
+                if (typeof (e.value) !== "undefined") {
+                    // @ts-ignore
+                    optStackTrace[e.key] = e.value
+                }
+            })
+        }
+
+        const getColor = (data: any, index: number) => {
+            let thisColor = opt.color[index]
+            let thisBackground = opt.secondColor[index]
+
+            if (thisBackground) {
+                // @ts-ignore
+                return chalk[thisColor][thisBackground](data)
+            }
+
+            if (thisColor) {
+                // @ts-ignore
+                return chalk[thisColor](data)
+            }
+
+            return data
+        }
+
+        const stackTraceKeys = Object.keys(optStackTrace)
+        const stackTraceLength = stackTraceKeys.length
+        let modifyCount = 0
+        let tmp
+
+        for (let i = 0; i < stackTraceLength; i++) {
+            const key = stackTraceKeys[i]
+            // @ts-ignore
+            const stackTraceDataKey = `${stackTraceData[key]}`
+            const divisor = getColor((i == (stackTraceLength - 1) ? " | " : " > "), 0)
+
+            // console.log(`[${key}] is the ${i == stackTraceLength? "last opt" : `n[${i}]` }`)
+            // console.log(i, "/", stackTraceLength -1)
+
+            // @ts-ignore
+            if (typeof (stackTraceData[key]) !== "undefined" && optStackTrace[key]) {
+                if (Array.isArray(data)) {
+                    if (modifyCount == 0) {
+                        tmp = (getColor(stackTraceDataKey, 0) + divisor)
+                    } else {
+                        tmp = (getColor(stackTraceDataKey, 0) + divisor + tmp)
+                    }
+                    if (i == (stackTraceLength - 1)) {
+                        let mix: any[] = []
+                        data.forEach((element: any) => {
+                            if (opt.color[1] || opt.secondColor[1]) {
+                                if (typeof (element) == "string") {
+                                    return mix.push(getColor(element, 1))
+                                }
                             }
-                        }
-                        mix.push(element)
-                    })
-                    mix.unshift(tmp)
-                    data = mix
+                            mix.push(element)
+                        })
+                        mix.unshift(tmp)
+                        data = mix
+                    }
+                } else {
+                    // @ts-ignore
+                    data = (getColor(stackTraceDataKey, 0) + divisor + getColor(data, 1))
                 }
-            } else {
-                // @ts-ignore
-                data = (getColor(stackTraceDataKey, 0) + divisor + getColor(data, 1))
+                modifyCount++
             }
-            modifyCount++
         }
-    }
 
-    if (Array.isArray(data)) {
+        if (Array.isArray(data)) {
+            // @ts-ignore
+            return console[opt.type](...data)
+        }
         // @ts-ignore
-        return console[opt.type](...data)
+        return console[opt.type](data)
+    } catch (error) {
+        return false
     }
-    // @ts-ignore
-    return console[opt.type](data)
 }
